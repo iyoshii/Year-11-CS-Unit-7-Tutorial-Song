0d5254da7adf79c7780618c75d020c92d86cee2b
yoshi <puggiedountsme@gmail.com>
2024-03-14 14:40:19 +0800

    Update 1.0

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private List<String> listeners;
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(List<String> newListeners){
+        int listenerCount = 0;
+        for(String listeners : newListeners){
+            String listenerName = listeners.toLowerCase();
+                if(!(this.listeners.contains(listenerName))){
+                    this.listeners.add(listenerName);
+                    listenerCount++;
+            }
+        }
+        return listenerCount;
+    }
 }
